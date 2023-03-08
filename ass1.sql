d50f07334bb8a081f4c184e3ccf421e6408aa8df
Luis Vicente Reyes <102662773+luisdlpr@users.noreply.github.com>
2023-03-08 22:08:41 +1100

    q2

--- ass1.sql
+++ ass1.sql
@@ -17,10 +17,21 @@ as
 -- Q2: beers that don't fit the ABV style guidelines
 
 -- put any Q2 helper views/functions here
+-- select b.name, s.max_abv, s.min_abv, b.abv from beers b full outer join styles s on b.style = s.id;
+-- select name from (select b.name, s.max_abv, s.min_abv, b.abv from beers b full outer join styles s on b.style = s.id) as v where abv > max_abv or abv < min_abv;
+create or replace view Beers_Styles_abv_details(name, max_abv, min_abv, abv) 
+as
+  SELECT b.name, s.max_abv, s.min_abv, b.abv
+  FROM beers b
+  FULL OUTER JOIN styles s
+  on b.style = s.id
+;
 
 create or replace view Q2(beer, style, abv, reason)
 as
-select null, null, null::ABVvalue, null  -- replace this with your SQL code
+  SELECT name 
+  FROM Beers_Styles_abs_details AS v
+  WHERE abv > max_abv OR abv < min_abv
 ;
 
 -- Q3: Number of beers brewed in each country
