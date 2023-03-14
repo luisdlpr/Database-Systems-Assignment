8201f93008375ee476ef8205b45c19c422532d6c
Luis Vicente Reyes <102662773+luisdlpr@users.noreply.github.com>
2023-03-14 23:41:43 +1100

    refactoring and styling

--- ass1.sql
+++ ass1.sql
@@ -1,10 +1,10 @@
 -- COMP3311 23T1 Assignment 1
+-- Luis Vicente De La Paz Reyes (z5206766)
+
 
 -- Q1: amount of alcohol in the best beers
-
 -- put any Q1 helper views/functions here
-
-CREATE OR REPLACE VIEW Q1 (beer, "sold in", alcohol)
+CREATE OR REPLACE VIEW Q1(beer, "sold in", alcohol)
 AS
   SELECT 
     name,
@@ -14,387 +14,328 @@ AS
   WHERE rating > 9
 ;
 
+
 -- Q2: beers that don't fit the ABV style guidelines
-
 -- put any Q2 helper views/functions here
-
+-- Beers (and their listed ABV's) mapped to the abv details of their styles.
 CREATE OR REPLACE VIEW Beer_ABV_details(name, style, max_abv, min_abv, abv)
 AS
   SELECT b.name, s.name, s.max_abv, s.min_abv, b.abv
-  FROM beers b
+  FROM Beers AS b
   FULL OUTER JOIN (
-    select *
-    from Styles) as s
-  on b.style = s.id
+    SELECT *
+    FROM Styles) AS s
+  ON b.style = s.id
 ;
 
-CREATE OR REPLACE function generate_reason(
+-- Formats the reason for an inacurate style into the required text for q2.
+CREATE OR REPLACE FUNCTION generate_reason(
   max_abv ABVvalue, 
   min_abv ABVvalue, 
   abv ABVvalue
-) returns text
-as $$
-  begin
-    if (abv < min_abv) then
-      return 'too weak by ' || (min_abv - abv)::numeric(3,1) || '%';
-    else
-      return 'too strong by ' || (abv - max_abv)::numeric(3,1) || '%';
-    end if;
-  end
-$$ language plpgsql;
+) RETURNS text AS 
+  $$
+    BEGIN
+      IF (abv < min_abv) THEN
+        RETURN 'too weak by ' || (min_abv - abv)::NUMERIC(3,1) || '%';
+      ELSE
+        RETURN 'too strong by ' || (abv - max_abv)::NUMERIC(3,1) || '%';
+      END IF;
+    END
+  $$ LANGUAGE plpgsql
+;
 
-create or replace view Q2(beer, style, abv, reason)
-as
+CREATE OR REPLACE VIEW Q2(beer, style, abv, reason)
+AS
   SELECT name, style, abv, generate_reason(max_abv, min_abv, abv)
   FROM Beer_ABV_details
   WHERE abv > max_abv OR abv < min_abv
 ;
 
 -- Q3: Number of beers brewed in each country
--- select c.name, count(*) from countries c left join (select b.name, l.within from breweries b inner join (select * from locations) as l on b.located_in = l.id) as i on i.within = c.id group by id;
--- select count(bby.brewery), brew.id from brewed_by as bby right join (select name, id from breweries) as brew on brew.id = bby.brewery group by brew.id;
-
--- ass1=# select l.located_in, sum(b.count) from breweries l left join numBeersByBrewery as b on b.brewery = l.located_in group by l.located_in;
--- ass1=# select max(sum) from (select l.located_in, sum(b.count) from breweries l left join numBeersByBrewery as b on b.brewery = l.located_in group by l.located_in) as b;
---  max
--- -----
---   56
--- (1 row)
-
--- ass1=# se(select l.located_in, sum(b.count) from breweries l left join numBeersByBrewery as b on b.brewery = l.located_in group by l.located_in);
--- ass1=# create view numBeersByBrewery(brewery, count) as select brewery, count(beer) from brewed_by group by brewery;
-
--- select br.located_in, sum(be.count) from breweries as br left join (select * from numBeersByBrewery) as be on be.brewery = br.id group by br.located_in;
--- create view breweryBeerCount(location, count) as select br.located_in, sum(be.count) from breweries as br left join (select * from numBeersByBrewery) as be on be.brewery = br.id group by br.located_in;
--- create view numBeersCountryNN(country, count) as select l.within, sum(count) from locations as l right join (select * from breweryBeerCount) as b on b.location = l.id group by l.within;
--- select c.name, coalesce(sum(b.count), 0) from countries c left join (select * from numBeersCountryNN) as b on b.country = c.id group by c.name;
--- CREATE VIEW
 -- -- put any Q3 helper views/functions here
-
-create or replace view B_id_by_L_id(b_id, brewery, l_id)
-as 
-select by.beer, br.name, br.located_in 
-from brewed_by by
-inner join (
-  select id, name, located_in
-  from breweries
-) as br
-on by.brewery = br.id
+-- Beers (ids) by their Brewery + that Breweries Location (id).
+CREATE OR REPLACE VIEW Beers_by_location(beer_id, brewery, location_id)
+AS 
+  SELECT _by.beer, br.name, br.located_in 
+  FROM Brewed_by AS _by
+  INNER JOIN (
+    SELECT id, name, located_in
+    FROM Breweries
+  ) AS br
+  ON _by.brewery = br.id
 ;
 
-create or replace view B_id_by_C_id(b_id, brewery, c_id)
-as
-select b.b_id, b.brewery, l.within 
-from B_id_by_L_id b 
-inner join (
-  select id, within
-  from locations
-) as l
-on l.id = b.l_id;
-
--- create or replace view beer_count_by_brewery(brewery, count)
--- as
--- select brewery, count(beer)
--- from brewed_by
--- group by brewery;
-
--- create or replace view beer_count_by_location(location, count)
--- as
--- select br.located_in, sum(be.count)
--- from breweries as br
--- left join (select * from beer_count_by_brewery) as be 
--- on be.brewery = br.id
--- group by br.located_in
--- ;
-
--- create or replace view beer_count_by_country_id(country, count)
--- as
--- select l.within, sum(count)
--- from locations as l
--- right join (
---   select *
---   from beer_count_by_location
--- ) as b
--- on b.location = l.id
--- group by l.within
--- ;
-
-create or replace view Q3(country, "#beers")
-as
-select c.name, coalesce(sum(b.count), 0)::bigint
-from countries c 
-left join (
-  select c_id, count(b_id) from B_id_by_C_id group by c_id
-) as b
-on b.c_id = c.id
-group by c.name-- replace this with your SQL code
+-- Beers (ids) and their Brewery + that Breweries Country (id).
+CREATE OR REPLACE VIEW Beers_by_country(beer_id, brewery, country_id)
+AS
+  SELECT b.beer_id, b.brewery, l.within 
+  FROM Beers_by_location AS b 
+  INNER JOIN (
+    SELECT id, within
+    FROM Locations
+  ) AS l
+  ON l.id = b.location_id
 ;
+
+CREATE OR REPLACE VIEW Q3(country, "#beers")
+AS
+  SELECT c.name, COALESCE(SUM(b.count), 0)::bigint
+  FROM Countries AS c 
+  LEFT JOIN (
+    SELECT country_id, COUNT(beer_id)
+    FROM Beers_by_country 
+    GROUP BY country_id
+  ) AS b
+  ON b.country_id = c.id
+  GROUP BY c.name
+  ;
 
 -- Q4: Countries where the worst beers are brewed
-
 -- put any Q4 helper views/functions here
--- create view ing_from_cze(name, id) as select i.name, i.id from ingredients i inner join (select c.id, c.code from countries c where c.code = 'CZE') as c on c.id = i.origin;
--- select by.beer, br.name, br.located_in from brewed_by by inner join (select id, name, located_in from breweries) br on by.brewery = br.id;
--- select b.beer, b.brewery_name, l.within from beer_by_location b inner join (select id, within from locations) as l on l.id = b.location;
--- select b.name, c.brewery_name, c.country from beer_by_country_id c inner join (select id, name from beers where rating < 3) b on b.id = c.beer;
--- select q.name, q.brewery, c.name from q4_cid q inner join (select id, name from countries) c on c.id = q.country;
+-- Beers (ids) and their countries (ids) filtered by rating < 3.
+CREATE OR REPLACE VIEW Worst_beers_by_country (beer, brewery, country_id)
+AS
+  SELECT b.name, c.brewery, c.country_id
+  FROM Beers_by_country AS c
+  INNER JOIN (
+    SELECT id, name
+    FROM Beers
+    WHERE rating < 3
+  ) AS b
+  ON b.id = c.beer_id
+  ;
 
-create or replace view B_id_by_C_id_filtered (beer, brewery, c_id)
-as
-select b.name, c.brewery, c.c_id
-from B_id_by_C_id c
-inner join (
-  select id, name
-  from beers
-  where rating < 3
-) as b
-on b.id = c.b_id
-;
-
-create or replace view Q4(beer, brewery, country)
-as
-select q.beer, q.brewery, c.name
-from B_id_by_C_id_filtered q
-inner join (
-  select id, name
-  from countries
-) c
-on c.id = q.c_id
+CREATE OR REPLACE VIEW Q4(beer, brewery, country)
+AS
+  SELECT q.beer, q.brewery, c.name
+  FROM Worst_beers_by_country AS q
+  INNER JOIN (
+    SELECT id, name
+    FROM Countries
+  ) AS c
+  ON c.id = q.country_id
 ;
 
 -- Q5: Beers that use ingredients from the Czech Republic
-
 -- put any Q5 helper views/functions here
-create or replace view ing_cze(i_id, name, itype, c_id)
-as
-select i.id, i.name, i.itype, i.origin
-from ingredients as i
-inner join (
-  select id
-  from countries
-  where countries.code = 'CZE'
-) as c
-on c.id = i.origin
+-- ingredients and some details from CZE.
+CREATE OR REPLACE VIEW Ingredients_CZE(ingredient_id, name, itype, country_id)
+AS
+  SELECT i.id, i.name, i.itype, i.origin
+  FROM Ingredients AS i
+  INNER JOIN (
+    SELECT id
+    FROM Countries
+    WHERE Countries.code = 'CZE'
+  ) AS c
+  ON c.id = i.origin
 ;
 
-create or replace view b_id_cze(b_id, ingredient, itype)
-as
-select c.beer, i.name, i.itype
-from contains c
-inner join (
-  select * from ing_cze
-) as i
-on i.i_id = c.ingredient;
+-- Beers (ids) made with the ingredients from CZE
+CREATE OR REPLACE VIEW Beers_CZE(beer_id, ingredient, itype)
+AS
+  SELECT c.beer, i.name, i.itype
+  FROM Contains AS c
+  INNER JOIN (
+    SELECT * FROM Ingredients_CZE
+  ) AS i
+  ON i.ingredient_id = c.ingredient
+;
 
-create or replace view Q5(beer, ingredient, "type")
-as
-select b.name, i.ingredient, i.itype::IngredientType  -- replace this with your SQL code
-from b_id_cze as i
-inner join (
-  select name, id
-  from beers
-) b 
-on b.id = i.b_id
+CREATE OR REPLACE VIEW Q5(beer, ingredient, "type")
+AS
+  SELECT b.name, i.ingredient, i.itype::IngredientType
+  FROM Beers_CZE AS i
+  INNER JOIN (
+    SELECT name, id
+    FROM Beers
+  ) b 
+  ON b.id = i.beer_id
 ;
 
 -- Q6: Beers containing the most used hop and the most used grain
-
 -- put any Q6 helper views/functions here
--- count occurences of each ingredient
--- incorporate itype here instead to filter and name
-create or replace view ing_freq(_count, ingredient)
-as
-select count(c.beer), c.ingredient
-from contains as c
-group by c.ingredient
+-- frequency of beers that use an ingredient.
+CREATE OR REPLACE VIEW Ingredient_frequency(_count, ingredient)
+AS
+  SELECT COUNT(c.beer), c.ingredient
+  FROM Contains AS c
+  GROUP BY c.ingredient
 ;
 
-create or replace view ing_freq_ext(_count, i_id, name, itype)
-as
-select f._count, f.ingredient, i.name, i.itype
-from ing_freq as f
-inner join (
-  select i.name, i.itype, i.id
-  from ingredients i
-) as i
-on f.ingredient = i.id
+-- extended details of the ingredients from Ingredient_frequency.
+CREATE OR REPLACE VIEW Ingredient_frequency_ext(
+  _count,
+  ingredient_id,
+  name,
+  itype
+) AS
+  SELECT f._count, f.ingredient, i.name, i.itype
+  FROM Ingredient_frequency AS f
+  INNER JOIN (
+    SELECT i.name, i.itype, i.id
+    FROM Ingredients i
+  ) AS i
+  ON f.ingredient = i.id
 ;
 
--- select most popular ing of each type
-create or replace view most_popular_by_type(id, itype)
-as
-select i_id, i.itype
-from ing_freq_ext as i
-right join (
-  select max(_count), itype
-  from ing_freq_ext
-  group by itype
-) as g 
-on g.max = i._count and g.itype = i.itype
+-- most popular ingredient of each type.
+CREATE OR REPLACE VIEW Most_popular_by_type(id, itype)
+AS
+  SELECT i.ingredient_id, i.itype
+  FROM Ingredient_frequency_ext AS i
+  RIGHT JOIN (
+    SELECT MAX(_count), itype
+    FROM Ingredient_frequency_ext
+    GROUP BY itype
+  ) AS g 
+  ON g.max = i._count AND g.itype = i.itype
 ;
 
-CREATE OR REPLACE function most_popular_itype(pop_i integer) returns setof integer
-as
-$$
-begin
-    return query
-    select c.beer
-    from contains c
-    where c.ingredient = pop_i;
-end;
-$$ language plpgsql
+-- function to return beers that contain the ingredient given.
+CREATE OR REPLACE FUNCTION Beers_with_ingredient(ingredient_id integer) 
+  RETURNS setof integer -- beer ids of relevant beers
+AS
+  $$
+  BEGIN
+      RETURN QUERY
+      SELECT c.beer
+      FROM Contains c
+      WHERE c.ingredient = ingredient_id;
+  END;
+  $$ LANGUAGE plpgsql
 ;
 
-create or replace view Q6(beer)
-as
-select b.name
-from beers b
-inner join (
-  select most_popular_itype(id) from most_popular_by_type where itype = 'hop'
-  intersect
-  select most_popular_itype(id) from most_popular_by_type where itype = 'grain'
-  -- select b_id from b_ids_w_pop_grain
-) as p 
-on p.most_popular_itype = b.id
+CREATE OR REPLACE VIEW Q6(beer)
+AS
+  SELECT b.name
+  FROM Beers b
+  INNER JOIN (
+    ( SELECT Beers_with_ingredient(id)
+      FROM Most_popular_by_type 
+      WHERE itype = 'hop'
+    ) INTERSECT (
+      SELECT Beers_with_ingredient(id) 
+      FROM Most_popular_by_type 
+      WHERE itype = 'grain' )
+  ) AS p 
+  ON p.Beers_with_ingredient = b.id
 ;
 
 -- Q7: Breweries that make no beer
-
 -- put any Q7 helper views/functions here
-
-create or replace view Q7(brewery)
-as
-select b.name
-from breweries b
-left join brewed_by by
-on by.brewery = b.id
-where by.brewery is null-- replace this with your SQL code
-;
+CREATE OR REPLACE VIEW Q7(brewery)
+AS
+  SELECT b.name
+  FROM Breweries b
+  LEFT JOIN brewed_by _by
+  ON _by.brewery = b.id
+  WHERE by.brewery IS NULL;
 
 -- Q8: Function to give "full name" of beer
-
 -- put any Q8 helper views/functions here
+-- beers and their corresponding brewery
+CREATE OR REPLACE VIEW Beers_by_brewery(beer_id, beer, brewery) AS
+SELECT b.id, b.name, br.brewery
+  FROM beers b
+  INNER JOIN (
+    SELECT * 
+    FROM Beers_by_location
+  ) AS br
+  ON br.beer_id = b.id;
 
--- create or replace view brewery_name_by_beer(beer, brewery_name) as
--- select by.beer, b.name
--- from brewed_by as by
--- inner join (
---   select id, name
---   from breweries
--- ) as b
--- on by.brewery = b.id
--- ;
+CREATE OR REPLACE FUNCTION
+        Q8(beer_id integer) RETURNS text
+AS
+  $$
+  DECLARE
+    result text;
+    beer text;
+    brewery text;
+  BEGIN
+    SELECT 
+      STRING_AGG(REGEXP_REPLACE(b.brewery, ' (Beer|Brew).*$', ''), ' + '), 
+      b.beer 
+      INTO brewery, beer
+    FROM Beers_by_brewery AS b
+    WHERE beer_id = b.beer_id
+    GROUP BY b.beer;
 
-create or replace view beer_breweries(b_id, beer, brewery) as
-select b.id, b.name, br.brewery
-from beers b
-inner join (
-  select * 
-  from B_id_by_L_id
-) as br
-on br.b_id = b.id;
+    IF beer IS NULL THEN
+      RETURN 'No such beer';
+    END IF;
 
--- given a beer id
--- obtain brewery: brewed by -> brewery.name, can use a helper view
--- process name
--- append beer name
--- select substring('Mountain Culture Beer Co' from '^(.*?)(Beer|Brew)')
-create or replace function
-        Q8(beer_id integer) returns text
-as
-$$
-declare
-  result text;
-  beer text;
-  brewery text;
-begin
-  select 
-    string_agg(REGEXP_REPLACE(b.brewery, ' (Beer|Brew).*$', ''), ' + '), 
-    b.beer 
-    into brewery, beer
-  from beer_breweries as b
-  where beer_id = b.b_id
-  group by b.beer;
-
-  if beer is null then
-    return 'No such beer';
-  end if;
-
-  result := brewery;
-
-  if result like '' then
     result := brewery;
-  end if;
 
-  result := result || ' ' || beer;
+    IF result LIKE '' THEN
+      result := b.brewery;
+    END IF;
 
-  return result;
+    result := result || ' ' || beer;
 
-
-end;
-$$ language plpgsql
+    RETURN result;
+  END;
+  $$ LANGUAGE plpgsql
 ;
 
 -- Q9: Beer data based on partial match of beer name
-
-drop type if exists BeerData cascade;
-create type BeerData as (beer text, brewer text, info text);
-
+DROP TYPE IF EXISTS BeerData CASCADE;
+CREATE TYPE BeerData AS (beer text, brewer text, info text);
 -- put any Q9 helper views/functions here
-create or replace view beer_ingredients(b_id, i_id, i_name, itype) as
-  select c.beer, c.ingredient, i.name, i.itype
-  from contains as c
-  inner join (
-    select *
-    from ingredients
-  ) as i
-  on i.id = c.ingredient
+-- beers and matching ingredients (essentially extended contains table).
+CREATE OR REPLACE VIEW Contains_ext(beer_id, ingredient_id, ingredient, itype) AS
+  SELECT c.beer, c.ingredient, i.name, i.itype
+  FROM Contains AS c
+  INNER JOIN (
+    SELECT *
+    FROM Ingredients
+  ) AS i
+  ON i.id = c.ingredient
 ;
 
-create or replace function
-        Q9(partial_name text) returns setof BeerData --BeerData
-as
-$$
-declare
-  m record;
-  breweries text;
-  itypes text[] := ARRAY['hop', 'grain', 'adjunct'];
-  labels text[] := ARRAY['Hops: ', 'Grain: ', 'Extras: '];
-  result text;
-  temp text;
-begin
-  for m in select b.name, b.id
-  from beers b
-  where b.name ~ ('(?i).*' || partial_name || '*')LOOP
+CREATE OR REPLACE FUNCTION Q9(partial_name text) returns setof BeerData
+AS
+  $$
+  DECLARE
+    m record;
+    breweries text;
+    itypes text[] := ARRAY['hop', 'grain', 'adjunct'];
+    labels text[] := ARRAY['Hops: ', 'Grain: ', 'Extras: '];
+    result text;
+    temp text;
+  BEGIN
+    FOR m IN SELECT b.name, b.id
+    FROM Beers b
+    WHERE b.name ~ ('(?i).*' || partial_name || '*')
+    LOOP
+      SELECT STRING_AGG(brewery, ' + ' order by brewery)
+      FROM Beers_by_brewery
+      WHERE beer_id = m.id
+      INTO breweries;
 
-    select string_agg(brewery, ' + ' order by brewery)
-    from beer_breweries
-    where b_id = m.id
-    into breweries;
+      result := '';
 
-    result := '';
+      FOR i IN 1..ARRAY_LENGTH(itypes, 1) LOOP
+        temp := NULL;
 
-    for i in 1..array_length(itypes, 1) LOOP
-      temp := null;
+        SELECT STRING_AGG(i_name, ',' ORDER BY ingredient)
+        FROM Contains_ext
+        WHERE beer_id = m.id AND itype::text = itypes[i] 
+        INTO temp;
 
-      select string_agg(i_name, ',' order by i_name)
-      from beer_ingredients
-      where b_id = m.id
-        and itype::text = itypes[i] 
-      into temp;
+        IF temp IS NOT NULL THEN
+          result := result || E'\n' || labels[i] || temp;
+        END IF;
+      END LOOP;
 
-      if temp is not null then
-        result := result || E'\n' || labels[i] || temp;
-      end if;
-    end loop;
+      IF SUBSTRING(result, 1, 1) = E'\n' THEN
+        result := SUBSTRING(result, 2);
+      END IF;
 
-    if substring(result, 1, 1) = E'\n' then
-      result := substring(result, 2);
-    end if;
-
-    return next (m.name, breweries, result)::BeerData;
-  end LOOP;
-end;
-$$ language plpgsql
+      RETURN NEXT (m.name, breweries, result)::BeerData;
+    END LOOP;
+  END;
+  $$ LANGUAGE plpgsql
 ;
 
